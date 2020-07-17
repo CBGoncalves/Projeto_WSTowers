a2903c44f884960d31fb80524a8d91630ae4bf9c
Vinicius Barbosa Go <cb5224144@gmail.com>
2020-07-17 08:23:39 -0300

    Projeto WSTowers com dictionary

---
+++ WSTowers/WSTowers/Models/Jogo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WSTowers.Models
+{
+    public class Jogo
+    {
+        public int idJogo { get; set; }
+        public string timeCasa { get; set; }
+        public string timeVisitante { get; set; }
+        public DateTime data { get; set; }
+
+        public override string ToString()
+        {
+            return $"{timeCasa}  X {timeVisitante} ";
+        }
+    }
+}
---
+++ WSTowers/WSTowers/Views/PrincipalView.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSTowers.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WSTowers.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    
+    public partial class PrincipalView : TabbedPage
+    {
+
+        public IList<Jogo> Jogos { get; private set; }
+        public PrincipalView()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            Jogos = new List<Jogo>();
+            Jogos.Add(new Jogo
+            {
+                idJogo = 1,
+                timeCasa = "São Paulo",
+                timeVisitante = "Goias",
+                data = Convert.ToDateTime("09/08/2020"),
+            });
+
+            Jogos.Add(new Jogo
+            {
+                idJogo = 2,
+                timeCasa = "Flamengo",
+                timeVisitante = "Atlético-MG",
+                data = Convert.ToDateTime("07/07/2020"),
+               
+
+            });
+
+            BindingContext = this;
+        }
+    }
+}
