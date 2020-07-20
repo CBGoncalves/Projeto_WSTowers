cf9ce8bb3db83e5f2d24e1559d246cbab90ee343
AnaLauraFeltrim <analaurafeltrim@gmail.com>
2020-07-20 16:13:25 -0300

    lista de jogadores

---
+++ WSTowers/WSTowers/Models/Elenco.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WSTowers.Models
+{
+    public class Elenco
+    {
+        public int IdJogador { get; set; }
+        public string Nome { get; set; }
+        public string Posicao { get; set; }
+        public int Numero { get; set; }
+        public string Time { get; set; }
+
+    }
+}
---
+++ WSTowers/WSTowers/Views/DetalheView.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSTowers.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WSTowers.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetalhesView : ContentPage
+    {
+        public IList<Elenco> Jogadores { get; set; }
+        public DetalhesView()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            Jogadores = new List<Elenco>();
+            Jogadores.Add(new Elenco
+            {
+                IdJogador = 1,
+                Nome= "Alexandre Pato",
+                Numero = 7,
+                Posicao = "Atacante",
+                Time = "São Paulo"
+            });
+
+            Jogadores.Add(new Elenco
+            {
+                IdJogador = 2,
+                Nome = "Tiago Volpi",
+                Numero = 1,
+                Posicao = "Goleiro",
+                Time = "São Paulo"
+            });
+
+            Jogadores.Add(new Elenco
+            {
+                IdJogador = 3,
+                Nome = "Daniel Alves",
+                Numero = 10,
+                Posicao = "Meia",
+                Time = "São Paulo"
+            });
+
+            Jogadores.Add(new Elenco
+            {
+                IdJogador = 4,
+                Nome = "Tchê Tchê",
+                Numero = 8,
+                Posicao = "Volante",
+                Time = "São Paulo"
+            });
+
+            Jogadores.Add(new Elenco
+            {
+                IdJogador = 5,
+                Nome = "Yago Rocha",
+                Numero = 5,
+                Posicao = "Volante",
+                Time = "Goias"
+            });
+
+            Jogadores.Add(new Elenco
+            {
+                IdJogador = 6,
+                Nome = "Marcelo Rangel",
+                Numero = 1,
+                Posicao = "Goleiro",
+                Time = "Goias"
+            });
+
+            Jogadores.Add(new Elenco
+            {
+                IdJogador = 7,
+                Nome = "Breno",
+                Numero = 7,
+                Posicao = "Meia",
+                Time = "Goias"
+            });
+
+            BindingContext = this;
+        }
+    }
+}
